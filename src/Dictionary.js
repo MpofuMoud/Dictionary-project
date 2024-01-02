b1171b224fb6ffada84bfa5c8e6a53feed89573b
Moud Mpofu <mpofunkazi@gmail.com>
2024-01-02 21:03:40 +0100

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https//api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+  //https://dictionaryapi.dev - documentation
+
   function handleKeywordChange(event) {
     console.log(event.target.value);
     setKeyword(event.target.value);
